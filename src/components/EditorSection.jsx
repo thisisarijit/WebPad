b9d7cd5ae36849dec5578e6e2d8dac6203247a28
Arijit Karmakar <arijitkarmakar873@gmail.com>
2026-09-09 13:04:37 +0530

    remove unused commented code

--- src/components/EditorSection.jsx
+++ src/components/EditorSection.jsx
@@ -1,22 +1,4 @@
-// import React, { useState, useEffect } from "react";
-// import LeftSideBar from "./LeftSideBar";
-// import { PanelLeft } from "lucide-react";
-
-// const EditorSection = () => {
-//   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-//   return (
-//     <div
-//       className={`h-screen w-screen grid ${isSidebarOpen ? "grid-cols-[1fr_5fr]" : "grid-cols-[1fr_9fr]"} gap-1 p-1`}
-//     >
-//       {isSidebarOpen ? <LeftSideBar /> : <PanelLeft />}
-//       <div className="border-2 rounded-lg">CodeAndOutput</div>
-//     </div>
-//   );
-// };
-
-// export default EditorSection;
-
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { PanelLeftOpen } from "lucide-react";
 import LeftSideBar from "./LeftSideBar";
 import CodeAndOutput from "./CodeAndOutput";
